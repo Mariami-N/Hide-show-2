6da392e13195c2d0f00e18f4f0879ee3e58e76a3
Mariami-N <avetianimari@gmail.com>
2023-02-15 10:50:00 -0800

    hide & show continue

---
+++ main.js
@@ -0,0 +1,12 @@
+let contaner=document.querySelector("contaner")
+let input=document.querySelector("input")
+let button=document.querySelector("button")
+
+
+// change.addEventListener("click", function () {
+//   orange.classList.toggle("gamochena");
+// });
+button.addEventListener("click", function(){
+input.classList.toggle("delete")
+
+})
